fa3cc91b40b74c0024579fc8144a3557f99c2739
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-07-22 21:09:42 +0530

    new style

--- src/ContactMe/ContactMe.jsx
+++ src/ContactMe/ContactMe.jsx
@@ -1,40 +1,62 @@
-import React from 'react'
-import styles from './ContactMe.module.scss'
-import Button from '../Button/Button'
+import React from "react";
+import styles from "./ContactMe.module.scss";
+import Button from "../Button/Button";
 export default function ContactMe() {
   return (
     <div id="contactme" className={styles.ContactMe}>
-      <div className={styles.left}>
-        <form action="https://formsubmit.co/kapilbadgujjar99@gmail.com" method="POST">
+      <div className={styles.ContactMeContainer}>
+        <div className={styles.left}>
+          <form
+            action="https://formsubmit.co/kapilbadgujjar99@gmail.com"
+            method="POST"
+          >
             <div>Get in touch</div>
-            <input type="text" name='user_name' placeholder='Your Name' required />
-            <input type="email" name='email' placeholder='email' required/>
-            <input type="text" name='name' placeholder='Phone Number' required/>
-            <textarea name='message' placeholder='Write a message...' required/>
-            <input type="submit" value="Submit"/>
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Your Name"
+              required
+            />
+            <input type="email" name="email" placeholder="email" required />
+            <input
+              type="text"
+              name="name"
+              placeholder="Phone Number"
+              required
+            />
+            <textarea
+              name="message"
+              placeholder="Write a message..."
+              required
+            />
+            <input type="submit" value="Submit" />
           </form>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.myDetails}>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.myDetails}>
             <h2>You can contact me here</h2>
             <div>
-                <h3>E-mail: </h3>
-                <hr/>
-                <a href="mailto:kapilbadgujjar99@gmail.com" ><h1>kapilbadgujjar99@gmail.com</h1></a>
+              <h3>E-mail: </h3>
+              <hr />
+              <a href="mailto:kapilbadgujjar99@gmail.com">
+                <h1>kapilbadgujjar99@gmail.com</h1>
+              </a>
             </div>
             <div>
-                <h3>Phone:</h3> 
-                <hr/>
-                <a href="tel: +91 7988220911" ><h1>+91 7988220911</h1></a>
+              <h3>Phone:</h3>
+              <hr />
+              <a href="tel: +91 7988220911">
+                <h1>+91 7988220911</h1>
+              </a>
             </div>
             <div>
-                <h3>Address: </h3>
-                <hr/>
-                <h2>VPO Jahazgarh, Jhajjar-124103, Haryana</h2>
+              <h3>Address: </h3>
+              <hr />
+              <h2>VPO Jahazgarh, Jhajjar-124103, Haryana</h2>
             </div>
-            
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
